8cc69b7a1f663e15ac99373d2ede11e223b6416a
samiksha-botre <botresamiksha1@gmail.com>
2026-08-04 14:46:47 +0530

    add user model and create users table

--- backend/models.py
+++ backend/models.py
@@ -8,3 +8,11 @@ class Medicine(Base):
     name = Column(String(100))
     company = Column(String(100))
     price = Column(String(20))
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(100), unique=True, index=True, nullable=False)
+    email = Column(String(100), unique=True, index=True, nullable=False)
+    password = Column(String(255), nullable=False)
